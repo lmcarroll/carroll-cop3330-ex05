81ee1b90c857cc044dd8f33ad8cd1ead7316b9d2
lmcarroll <liamcarrollapplicant@gmail.com>
2021-09-07 18:52:53 -0400

    Added all code

--- src/main/java/SimpleMath.java
+++ src/main/java/SimpleMath.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class SimpleMath {
+import java.util.Scanner;
+
+public class SimpleMath {
+    public static void main(String[] args) {
+        System.out.println("What is the first number?");
+        Scanner input = new Scanner(System.in);
+        String NumString = input.nextLine();
+        int FirstNumInt = Integer.parseInt(NumString);
+
+        System.out.println("What is the second number?");
+        input = new Scanner(System.in);
+        NumString = input.nextLine();
+        int SecondNumInt = Integer.parseInt(NumString);
+
+        int summation = FirstNumInt + SecondNumInt;
+        int difference = FirstNumInt - SecondNumInt;
+        int product = FirstNumInt * SecondNumInt;
+        int quotient = FirstNumInt / SecondNumInt;
+
+        System.out.println(FirstNumInt + "+" + SecondNumInt + "=" + summation + "\n"
+                + FirstNumInt + "-" + SecondNumInt + "=" + difference + "\n"
+                + FirstNumInt + "*" + SecondNumInt + "=" + product + "\n"
+                + FirstNumInt + "/" + SecondNumInt + "=" + quotient + "\n");
+    }
 }
